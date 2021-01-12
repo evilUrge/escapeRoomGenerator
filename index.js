7939036d4fc4719bbe6cd6217a362d57918128d9
Gilad Maoz <gilad@maoz.dev>
2021-01-12 12:29:23 +0100

    - Included a constant for better string n' regex management  - Supports media hints (youtube, images and links)  refactored some of the mess
     mooi

--- index.js
+++ index.js
@@ -8,84 +8,72 @@ const chalk = require('chalk'),
         strSlicer,
         qr,
         generatePage
-    } = require("./src/utils");
+    } = require("./src/utils"),
+    {
+        regexConst,
+        embed,
+        textConst
+    } = require("./src/constant")
 require('clear')()
-console.log(
-    chalk.yellow(
-        require('figlet').textSync('EscapeRoomGenerator!',)
-    )
-);
-console.log(chalk.bgBlueBright.whiteBright.bold('Welcome to Escape Room Generator\nMake the best out of COVID19 and surprise your housemate/lover/dog!'))
 
-if (process.env.NODE_ENV) {
-    const output = {
-        name: 'demo 321',
-        baseURL: 'http://www.kekeke.net/',
-        riddles: { //Made up hint to the first one
-            '9d4f6a44d190fc5f': 'eifo ze',
-            '4da5ef350f91c238': 'ze po?',
-            '4ebf3767db2a6e7b': 'lama ?',
-            'fin_36a0d89f033b952f': 'kol hakavod'
-        }
-    }
-    //STARTS CUT HERE
-    folderInit(Object.keys(output.riddles).filter(id => !id.startsWith('fin_')))
-    const baseURL = output.baseURL.endsWith('/') ? output.baseURL : `${output.baseURL}/`
-    const keys = generateGPG()
-    const finalId = Object.keys(output.riddles).find(i => i.startsWith('fin_'))
-    const finalEncSplit = strSlicer(encryptStringWithRsaPublicKey(output.riddles[finalId]), Object.keys(output.riddles).length - 1)
-    const qrCodes = []
-    for (let i = 0; i < Object.keys(output.riddles).length - 1; i += 1) {
-        const id = Object.keys(output.riddles)[i]
-        qrCodes.push(qr(`${baseURL}${id}`).then(qrImg => {
-                generatePage(output.name, id, finalEncSplit[i], output.riddles[id], qrImg)
-                return qrImg
-            }
-        ))
-    }
-    // await Promise.all(qrCodes)
-    // TILL HERE
-} else {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Give your escape room a snappy name:',
-            validate: (value) => value ? true : chalk.red('Please enter a name!')
-        },
-        {
-            type: 'input',
-            name: 'baseURL',
-            message: `What's the base URL you gonna use for this online experience (soon gonna auto upload to firebase).`,
-            validate: (value) => value.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/) ? true : chalk.red('Please enter a valid URL!')
-        },
-        {
-            type: 'input',
-            name: 'riddles',
-            message: "How many steps would you like to generate (number of riddles)",
-            validate: (value) => value.match(/^[0-9]*$/i) ? true : chalk.red('Please enter a valid number!')
-        }])
-        .then(async answers => {
-            const questions = []
-            for (let i = 0; i < parseInt(answers.riddles); i += 1) {
-                questions.push({
-                    type: 'input',
-                    name: generateId(),
-                    message: `Question ${i + 1}:`,
-                    validate: (value) => value ? true : chalk.red('No text')
-                });
-            }
-            questions.push({
+console.log(chalk.yellow(require('figlet').textSync(textConst.name)));
+console.log(chalk.bgBlueBright.whiteBright.bold(textConst.welcome))
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: textConst.wizard.name,
+        validate: (value) => value ? true : chalk.red(textConst.error.name)
+    },
+    {
+        type: 'input',
+        name: 'baseURL',
+        message: textConst.wizard.url,
+        validate: (value) => value.match(regexConst['External link']) ? true : chalk.red(textConst.error.url)
+    },
+    {
+        type: 'input',
+        name: 'riddles',
+        message: textConst.wizard.steps,
+        validate: (value) => value.match(regexConst.int) ? true : chalk.red(textConst.error.int)
+    }])
+    .then(async answers => {
+        const riddles = {}
+        const ask = async (num, format, regEx) =>
+            await inquirer.prompt({
                 type: 'input',
-                name: `fin_generateId()`,
-                message: `Money time! Give the last hint to where you hid your present`,
-                validate: (value) => value ? true : chalk.red('No text')
+                name: 'question',
+                format: {
+                    'Text': 'text', 'External link': 'extlink',
+                    'YouTube': 'youtube', 'Image': 'img'
+                }[format],
+                message: `Hint #${num}:`,
+                validate: (value) => value.match(regEx) ? true : chalk.red(textConst.error.input)
+            }).then(val =>
+                Object.assign(val, {format: format, name: generateId()}))
+        for (let i = 0; i < parseInt(answers.riddles); i += 1) {
+            const riddle = await inquirer.prompt({
+                type: 'list',
+                name: 'type',
+                message: textConst.wizard.hintType,
+                choices: ['Text', 'External link', 'YouTube', 'Image'],
+            }).then(async typeOfRiddle => {
+                console.log(textConst.typeOfRiddle[typeOfRiddle.type])
+                return await ask(i + 1, typeOfRiddle.type, regexConst[typeOfRiddle.type])
             })
-            return Object.assign(answers, {riddles: await inquirer.prompt(questions)})
-        }).then(async output => {
-
-
-        // HERE!
+            riddles[riddle.name] = {value: riddle.question, format: riddle.format}
+        }
+        return Object.assign(answers, {
+            riddles: Object.assign(riddles, await inquirer.prompt({
+                type: 'input',
+                name: `fin_${generateId()}`,
+                message: textConst.wizard.lastHint,
+                validate: (value) => value ? true : chalk.red(textConst.error.noText)
+            }))
+        })
+    })
+    .then(async output => {
         folderInit(Object.keys(output.riddles).filter(id => !id.startsWith('fin_')))
         const baseURL = output.baseURL.endsWith('/') ? output.baseURL : `${output.baseURL}/`
         const keys = generateGPG()
@@ -93,15 +81,18 @@ if (process.env.NODE_ENV) {
         const finalEncSplit = strSlicer(encryptStringWithRsaPublicKey(output.riddles[finalId]), Object.keys(output.riddles).length - 1)
         const qrCodes = []
         for (let i = 0; i < Object.keys(output.riddles).length - 1; i += 1) {
+            console.log(output.riddles[Object.keys(output.riddles)[i]])
+            console.log(embed[output.riddles[Object.keys(output.riddles)[i]].format])
             const id = Object.keys(output.riddles)[i]
             qrCodes.push(qr(`${baseURL}${id}`).then(qrImg => {
-                    generatePage(output.name, id, finalEncSplit[i], output.riddles[id], qrImg, i + 1, Object.keys(output.riddles).length - 1)
+                    generatePage(output.name, id, finalEncSplit[i],
+                        embed[output.riddles[id].format](output.riddles[id].value),
+                        qrImg, i + 1, Object.keys(output.riddles).length - 1)
                     return qrImg
                 }
             ))
         }
         await Promise.all(qrCodes)
-        console.log(chalk.green.bold('Mazal tof! check your /public folder for the your indoor escape room!'))
-        console.log('In addition, we included the GPG keys used to encode the final message + a pdf file with all the QR codes for you to hide around the house!')
+        textConst.finale.forEach(msg => console.log(chalk.green.bold(msg)))
     })
-}
+
---
+++ src/constant.js
@@ -0,0 +1,42 @@
+module.exports = {
+    regexConst: {
+        any: /.*/,
+        int: /^[0-9]*$/i,
+        'Text': /.*/,
+        'External link': /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/,
+        'Image': /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/,
+        'YouTube': /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/,
+    },
+    embed: {
+        'YouTube': (url) => `<iframe width="420" height="345" src="${url}"></iframe>`,
+        'Image': (url) => `<img width="420" src="${url}"/>`,
+        'External link': (url) => `<a href="${url}">${url}</a>`,
+        'Text': (text) => `<h5>${text}</h5>`
+    },
+    textConst: {
+        name: 'EscapeRoomGenerator!',
+        welcome: 'Welcome to Escape Room Generator\nMake the best out of COVID19 and surprise your housemate/lover/dog!',
+        wizard: {
+            name: 'Give your escape room a snappy name:',
+            url: `What's the base URL you gonna use for this online experience (soon gonna auto upload to firebase).`,
+            steps: 'How many steps would you like to generate (number of riddles)',
+            hintType: 'Select the type of hint',
+            lastHint: 'Money time! Give the last hint to where you hid your present'
+        },
+        error: {
+            name: 'Please enter a name!',
+            url: 'Please enter a valid URL!',
+            int: 'Please enter a valid number!',
+            input: 'Invalid input!',
+            noText: 'No text'
+        },
+        typeOfRiddle: {
+            'Text': 'Free string, you can type any clue you want in UTF8',
+            'External link': 'Use a valid hyperlink and send them to a quest',
+            'YouTube': 'Let them watch something',
+            'Image': 'All image format supported'
+        },
+        finale: ['Mazal tof! check your /public folder for the your indoor escape room!',
+            'In addition, we included the GPG keys used to encode the final message + a pdf file with all the QR codes for you to hide around the house!']
+    }
+}
